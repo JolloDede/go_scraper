6a29311237343cbf79c59081be5a09e255ca1230
Dennis Lee <dl@aduna.ch>
2025-02-25 15:28:02 +0100

    backup

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# Todo
+
+- [ ] Make the Data more concice
+  - [ ] save the current page url in unsearched
+  - [ ] write to current page to saved
+- [ ] Output Report
--- src/handle_url.go
+++ src/handle_url.go
@@ -10,15 +10,15 @@ import (
 	"golang.org/x/net/html"
 )
 
-func HandleUrl(url string, recursiv bool) map[string]int {
+func HandleUrl(url string, recursiv bool) map[string]Searched {
 	scraper := NewUrlScraper(url)
 
-	scraper.checkUrl(url)
+	scraper.checkUrl(ToSearch{currentPath: url, linkUrl: url})
 	if recursiv {
 		for len(scraper.unsearched) != 0 {
-			var x string
+			var x ToSearch
 			x, scraper.unsearched = scraper.unsearched[len(scraper.unsearched)-1], scraper.unsearched[:len(scraper.unsearched)-1]
-			fmt.Println("Check URL: ", x)
+			fmt.Println("Check URL: ", x.linkUrl)
 			scraper.checkUrl(x)
 		}
 	}
@@ -26,11 +26,21 @@ func HandleUrl(url string, recursiv bool) map[string]int {
 	return scraper.searched
 }
 
+type ToSearch struct {
+	currentPath string
+	linkUrl     string
+}
+
+type Searched struct {
+	path       string
+	statusCode int
+}
+
 type UrlScraper struct {
 	scheme     string
 	host       string
-	searched   map[string]int
-	unsearched []string
+	searched   map[string]Searched
+	unsearched []ToSearch
 }
 
 func NewUrlScraper(u string) *UrlScraper {
@@ -44,62 +54,62 @@ func NewUrlScraper(u string) *UrlScraper {
 	res.Path = ""
 	res.RawQuery = ""
 	res.RawFragment = ""
-	return &UrlScraper{scheme: res.Scheme, host: res.Host, searched: make(map[string]int, 0), unsearched: []string{p}}
+	return &UrlScraper{scheme: res.Scheme, host: res.Host, searched: make(map[string]Searched, 0), unsearched: []ToSearch{{currentPath: p, linkUrl: p}}}
 }
 
-func (s *UrlScraper) checkUrl(u string) {
-	res, err := http.Get(u)
+func (s *UrlScraper) checkUrl(u ToSearch) {
+	res, err := http.Get(u.linkUrl)
 
 	if err != nil {
-		s.searched[u] = 404
+		s.searched[u.linkUrl] = Searched{path: u.currentPath, statusCode: 404}
 		return
 	}
 	defer res.Body.Close()
 
-	if sameHost(s.scheme+"://"+s.host, u) {
-		s.parseBody(res.Body)
+	if sameHost(s.scheme+"://"+s.host, u.linkUrl) {
+		s.parseBody(u.linkUrl, res.Body)
 	}
 
 	if slices.Contains([]int{300, 303}, res.StatusCode) {
 		locations := res.Header["Location"]
 		for i := 0; i < len(locations); i++ {
-			s.checkUrl(locations[i])
+			s.checkUrl(ToSearch{currentPath: u.linkUrl, linkUrl: locations[i]})
 		}
 	}
 
 	if slices.Contains([]int{301, 302, 305, 307, 308}, res.StatusCode) {
 		location := res.Request.Response.Header["Location"]
-		s.checkUrl(location[0])
+		s.checkUrl(ToSearch{currentPath: u.linkUrl, linkUrl: location[0]})
 	}
 
-	if s.searched[u] == 0 {
-		s.searched[u] = res.StatusCode
+	if s.searched[u.currentPath].statusCode == 0 {
+		s.searched[u.currentPath] = Searched{path: u.currentPath, statusCode: res.StatusCode}
 	}
 }
 
-func (s *UrlScraper) parseBody(body io.ReadCloser) {
+func (s *UrlScraper) parseBody(cur string, body io.ReadCloser) {
 	doc, err := html.Parse(body)
 
 	if err != nil {
 		return
 	}
 
-	s.traverseRec(doc)
+	s.traverseRec(cur, doc)
 }
 
-func (s *UrlScraper) traverseRec(node *html.Node) {
+func (s *UrlScraper) traverseRec(cur string, node *html.Node) {
 	for n := range node.Descendants() {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for i := 0; i < len(n.Attr); i++ {
 				if n.Attr[i].Key == "href" {
-					s.addToUnsearched(n.Attr[i].Val)
+					s.addToUnsearched(cur, n.Attr[i].Val)
 				}
 			}
 		}
 	}
 }
 
-func (s *UrlScraper) addToUnsearched(p string) {
+func (s *UrlScraper) addToUnsearched(cur string, p string) {
 	u, err := url.Parse(p)
 
 	if err != nil {
@@ -116,12 +126,12 @@ func (s *UrlScraper) addToUnsearched(p string) {
 	}
 
 	if !s.pathAlreadySearched(u.String()) {
-		s.unsearched = append(s.unsearched, u.String())
+		s.unsearched = append(s.unsearched, ToSearch{currentPath: cur, linkUrl: u.String()})
 	}
 }
 
 func (s *UrlScraper) pathAlreadySearched(p string) bool {
-	if s.searched[p] == 0 {
+	if s.searched[p].statusCode == 0 {
 		return false
 	} else {
 		return true
